8d969fa9405278089163a3507315198ffd8157a0
Siddy098 <104010240+Siddy098@users.noreply.github.com>
2022-09-03 11:19:51 +0530

    Add files via upload

---
+++ Assignment4.js
@@ -0,0 +1,10 @@
+//Assignment 4
+
+var matchHouses = (step) => {
+    var result = (step * 6) - (step - 1)
+    return result
+}
+
+console.log(matchHouses(1))
+console.log(matchHouses(4))
+console.log(matchHouses(87))
